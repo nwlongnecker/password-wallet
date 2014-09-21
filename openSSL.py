ec3a0f90b5218f27b87b13a845ba6ac5d4b1c5d7
ryandanas <ryandanas@gmail.com>
2014-09-21 18:51:10 -0400

    Got rid of globals. Encrypt should be close to done.

--- openSSL.py
+++ openSSL.py
@@ -3,26 +3,23 @@
 import fileIO
 import subprocess
 
-INPUT_FILE = "in.tmp"
-OUTPUT_FILE = "out.tmp"
-
 # Hashes the input to a key
 # @param String plaintext The string to hash
 # @return Returns the hashed key
 def hash(plaintext):
 	# create temp files
-	fileIO.createFile(INPUT_FILE)
-	fileIO.createFile(OUTPUT_FILE)
+	fileIO.createFile('plain.tmp')
+	fileIO.createFile('hash.tmp')
 	# write out the input
-	fileIO.writeFile(INPUT_FILE, plaintext)
+	fileIO.writeFile('plain.tmp', plaintext)
 	# run openssl hash command
 	# TODO: strip SHA256(in.tmp)=... from string
-	subprocess.check_output(['openssl', 'dgst', '-sha256', '-hex', '-out', OUTPUT_FILE, INPUT_FILE], stderr=subprocess.STDOUT)
+	subprocess.check_output(['openssl', 'dgst', '-sha256', '-hex', '-out', 'hash.tmp', 'plain.tmp'], stderr=subprocess.STDOUT)
 	# read in the output
-	hashKey = fileIO.readFile(OUTPUT_FILE)
+	hashKey = fileIO.readFile('hash.tmp')
 	# delete temp files
-	fileIO.removeFile(INPUT_FILE)
-	fileIO.removeFile(OUTPUT_FILE)
+	fileIO.removeFile('plain.tmp')
+	fileIO.removeFile('hash.tmp')
 	# return the output
 	return hashKey
 
@@ -39,8 +36,24 @@ def decrypt(key, cipher):
 # @param String plaintext The text to encrypt
 # @return Returns the encrypted plaintext
 def encrypt(key, plaintext):
-	# print('Encrypted ' + plaintext + ' with key ' + key)
-	return plaintext
+	# create temp files
+	fileIO.createFile('key.tmp')
+	fileIO.createFile('plain.tmp')
+	fileIO.createFile('cipher.tmp')
+	# write out the input
+	fileIO.writeFile('key.tmp', key)	
+	fileIO.writeFile('plain.tmp', plaintext)
+	# run openssl enc command
+	#openssl enc -aes-256-cbc -pass stdin -in infile.txt -out this.txt
+	subprocess.check_output(['openssl', 'enc', '-aes-256-cbc', '-pass', 'key.tmp', '-in', 'plain.tmp', '-out', 'cipher.tmp'], stderr=subprocess.STDOUT)
+	# read in the output
+	ciphertext = fileIO.readFile('cipher.tmp')
+	# delete temp files
+	fileIO.removeFile('key.tmp')
+	fileIO.removeFile('plain.tmp')
+	fileIO.removeFile('cipher.tmp')
+	# return the output
+	return ciphertext
 
 # Generates a random password
 # @return Returns the generated password
