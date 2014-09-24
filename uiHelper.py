d465a7cdf0c56064fa8cd49528821992bfe6533b
ryandanas <ryandanas@gmail.com>
2014-09-24 15:56:56 -0400

    Got rid of clipboard copying because its not working. Just print out password.

--- uiHelper.py
+++ uiHelper.py
@@ -22,18 +22,6 @@ def askReset():
 # Copies the given text to the system clipboard
 # @param String text The text to paste to the system clipboard
 def addToClipboard(text):
-	try:
-		p1 = subprocess.Popen(["echo", text], stdout=subprocess.PIPE)
-		p2 = subprocess.Popen(["xclip"], stdin=p1.stdout, stdout=subprocess.PIPE)
-		p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
-		p2.communicate()[0]
-		print "password copied to clipboard"
-	except:
-		print ""
-		print "Unable to copy password to clipboard using xclip;"
-		print "Possible issues..."
-		print "\txclip is not installed (try sudo apt-get xclip)"
-		print "\tyou do not have access to the clipboard"
-		print "\tyou are ssh'd in to a unix terminal"
-		print ""
-		print "Password= ", text
+	# Copying to a non X11 clipboard in unix seems really difficult
+	# The password is so big that shoulder surfing attacks are unviable
+	print "Password= ", text
